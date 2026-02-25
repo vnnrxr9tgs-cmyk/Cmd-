15f4b1be70c4fc80121cf726fbc9bc527b6d2029
Igor <vnnrxr9tgs@privaterelay.appleid.com>
2026-02-25 20:52:05 +1000

    Update promet.py

--- promet.py
+++ promet.py
@@ -46,5 +46,44 @@ if __name__ == "__main__":
         collect_metrics()
         time.sleep(5)
 
+# Имя хоста
+HOSTNAME = socket.gethostname()
+
+process_uptime = Gauge(
+    "process_uptime_seconds",
+    "Время работы процесса",
+    ["host", "process", "pid"]
+)
+
+def collect_metrics():
+    now = time.time()
+    process_uptime.clear()
+
+    for proc in psutil.process_iter(['pid', 'name', 'create_time']):
+        try:
+            name = proc.info['name']
+
+            if name in TARGET_PROCESSES:
+                uptime = now - proc.info['create_time']
+
+                process_uptime.labels(
+                    host=HOSTNAME,
+                    process=name,
+                    pid=str(proc.info['pid'])
+                ).set(uptime)
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+
+
+if __name__ == "__main__":
+    start_http_server(8000)
+    print("Exporter started on port 8000")
+
+    while True:
+        collect_metrics()
+        time.sleep(5)
+
 pyinstaller --onefile --clean --noconsole exporter.py
+
 process_uptime_seconds
